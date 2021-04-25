81ecf788318344b83758db016b9042b3170b7972
Sujeet <sujeet9271@gmail.com>
2021-04-25 15:56:32 +0545

    corsheader update

--- food_delivery/settings.py
+++ food_delivery/settings.py
@@ -113,7 +113,7 @@ REST_FRAMEWORK = {
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "https://foodeliverybhw.herokuapp.com/"
+    "https://foodeliverbhw.herokuapp.com"
 ]
 
 
