fadfd2610986b164988e77c1b0e4eb1bda6843d5
Sujeet <sujeet9271@gmail.com>
2021-04-25 15:43:45 +0545

    corsheader update

--- food_delivery/settings.py
+++ food_delivery/settings.py
@@ -108,12 +108,9 @@ REST_FRAMEWORK = {
 }
 
 # LOGIN_REDIRECT_URL='/next/'
-# CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_ALL_ORIGINS = True
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000"
-]
+
 
 
 SIMPLE_JWT = {
